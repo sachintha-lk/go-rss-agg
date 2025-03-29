cbba26fa104334dac9a60577fdf3cf25da164acf
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2025-03-29 12:25:48 +0530

    feat: add user api auth

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/sachintha-lk/go-rss-agg/internal/auth"
 	"github.com/sachintha-lk/go-rss-agg/internal/database"
 )
 
@@ -34,6 +35,24 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %s", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+
+	apiKey, err := auth.GetAPIKey(r.Header)
+
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth Error: %s", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Couldn't get user: %s", err))
 		return
 	}
 
